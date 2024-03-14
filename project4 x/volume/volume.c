1dfcf0bce95890238fda9ec52048d0b6dfa2ea46
AiakosXD <104672600+Aiakos13@users.noreply.github.com>
2024-03-14 21:39:21 +0330

    Add files via upload

---
+++ project4 x/volume/volume.c
@@ -0,0 +1,53 @@
+#include <cs50.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+// Number of bytes in .wav header
+const int HEADER_SIZE = 44;
+
+int main(int argc, char *argv[])
+{
+    // Check command-line arguments
+    if (argc != 4)
+    {
+        printf("Usage: ./volume input.wav output.wav factor\n");
+        return 1;
+    }
+
+    // Open files and determine scaling factor
+    FILE *input = fopen(argv[1], "r");
+    if (input == NULL)
+    {
+        printf("Could not open file.\n");
+        return 1;
+    }
+
+    FILE *output = fopen(argv[2], "w");
+    if (output == NULL)
+    {
+        printf("Could not open file.\n");
+        return 1;
+    }
+
+    float factor = atof(argv[3]);
+
+    // TODO: Copy header from input file to output file
+    uint8_t header[HEADER_SIZE];
+    /* There is only one header, so parse it just once */
+    fread(&header, sizeof(header), 1, input);
+    fwrite(&header, sizeof(header), 1, output);
+
+    // TODO: Read samples from input file and write updated data to output file
+    int16_t buffer;
+    while (fread(&buffer, sizeof(buffer), 1, input))
+    {
+        buffer *= factor;
+        fwrite(&buffer, sizeof(buffer), 1, output);
+    }
+
+    // Close files
+    fclose(input);
+    fclose(output);
+}
